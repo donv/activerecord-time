bbc893ece26e3ad0231b4ff30c726d7ea6411048
Uwe Kubosch <uwe@kubosch.no>
2016-12-23 21:21:16 +0100

    Improved travis matrix script

--- matrix_test.rb
+++ matrix_test.rb
@@ -7,8 +7,34 @@ update_gemfiles = ARGV.delete('--update')
 require 'yaml'
 travis = YAML.load(File.read('.travis.yml'))
 
+def run_script(ruby, env, gemfile)
+  env.scan(/\b(?<key>[A-Z_]+)="(?<value>.+?)"/) do |key, value|
+    ENV[key] = value
+  end
+  puts '*' * 80
+  puts "Testing #{ruby} #{gemfile} #{env}"
+  puts
+  system "chruby-exec #{ruby} -- bundle exec rake" || exit(1)
+  puts '*' * 80
+end
+
+def use_gemfile(ruby, gemfile, update_gemfiles)
+  puts '$' * 80
+  puts "Testing #{gemfile}"
+  puts
+  ENV['BUNDLE_GEMFILE'] = gemfile
+  if update_gemfiles
+    system "chruby-exec #{ruby} -- bundle update"
+  else
+    system "chruby-exec #{ruby} -- bundle check || chruby-exec #{ruby} -- bundle install"
+  end || exit(1)
+  yield
+  puts "Testing #{gemfile} OK"
+  puts '$' * 80
+end
+
 travis['rvm'].each do |ruby|
-  next if ruby =~ /head/
+  next if ruby =~ /head/ # ruby-install does not support HEAD installation
   puts '#' * 80
   puts "Testing #{ruby}"
   puts
@@ -16,31 +42,16 @@ travis['rvm'].each do |ruby|
   bundler_gem_check_cmd = "chruby-exec #{ruby} -- gem query -i -n bundler >/dev/null"
   system "#{bundler_gem_check_cmd} || chruby-exec #{ruby} -- gem install bundler" || exit(1)
   travis['gemfile'].each do |gemfile|
-    if travis['matrix']['exclude'].any? { |f| f['rvm'] == ruby && f['gemfile'] == gemfile }
+    if (travis['matrix']['exclude'].to_a + travis['matrix']['allowed_failures'].to_a)
+        .any? { |f| f['rvm'] == ruby && f['gemfile'] == gemfile }
       puts 'Skipping known failure.'
       next
     end
-    puts '$' * 80
-    puts "Testing #{gemfile}"
-    puts
-    ENV['BUNDLE_GEMFILE'] = gemfile
-    if update_gemfiles
-      system "chruby-exec #{ruby} -- bundle update"
-    else
-      system "chruby-exec #{ruby} -- bundle check || chruby-exec #{ruby} -- bundle install"
-    end || exit(1)
-    travis['env'].each do |env|
-      env.scan(/\b(?<key>[A-Z_]+)="(?<value>.+?)"/) do |key, value|
-        ENV[key] = value
+    use_gemfile(ruby, gemfile, update_gemfiles) do
+      travis['env'].each do |env|
+        run_script(ruby, env, gemfile)
       end
-      puts '*' * 80
-      puts "Testing #{ruby} #{gemfile} #{env}"
-      puts
-      system "chruby-exec #{ruby} -- bundle exec rake" || exit(1)
-      puts '*' * 80
     end
-    puts "Testing #{gemfile} OK"
-    puts '$' * 80
   end
   puts "Testing #{ruby} OK"
   puts '#' * 80
